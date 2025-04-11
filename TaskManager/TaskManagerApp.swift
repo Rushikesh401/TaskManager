ed27d26f88632509025839726c64dc2db36f272d
Rushikesh Suradhkar <rushikeshsuradkar2000@gmail.com>
2025-04-11 12:33:48 +0530

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Rushikesh Suradkar on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
